912e81cae6e427e494821f2e9ae5d3679da48b4a
Aniketh Maddipati <aniketh@anikeths-mbp.lan>
2020-07-18 15:34:58 -0400

    creating one of a few empty components to work with

---
+++ src/components/navbar.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const navbar = () => {
+    return (
+        <nav class="navbar navbar-light bg-light">
+  <a class="navbar-brand" href="#">Job Board</a>
+</nav>
+    );
+ }
+export default navbar;
