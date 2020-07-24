b3cba1aeb648f28f957b15e15b2cfaf0f357cc0b
Aniketh Maddipati <aniketh@anikeths-mbp.lan>
2020-07-24 13:20:50 -0400

    Updates

---
+++ precache-manifest.8c51a9ede9ae46080ec918cd7ea05cda.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ba77ffc3f81f0f12addd33e2024f2bd",
+    "url": "/h1b-job-board-frontend/index.html"
+  },
+  {
+    "revision": "6ad699b6096f5ce253c9",
+    "url": "/h1b-job-board-frontend/static/css/main.d7cd84bf.chunk.css"
+  },
+  {
+    "revision": "9c7be1debdadc12e58ab",
+    "url": "/h1b-job-board-frontend/static/js/2.2cbf0697.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/h1b-job-board-frontend/static/js/2.2cbf0697.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ad699b6096f5ce253c9",
+    "url": "/h1b-job-board-frontend/static/js/main.db02d51b.chunk.js"
+  },
+  {
+    "revision": "b21f27337caa192e47bb",
+    "url": "/h1b-job-board-frontend/static/js/runtime-main.85241da4.js"
+  }
+]);
